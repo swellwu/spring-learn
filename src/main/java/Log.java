87f9df1e9ab6d8df2b9607556f96879eb9a447ca
swellwu <heng_0577@163.com>
2017-02-05 08:37:39 +0800

    aop简单示例

---
+++ src/main/java/Log.java
@@ -0,0 +1,14 @@
+/**
+ * Created by wuxinjian on 2017/2/5.
+ */
+public class Log {
+
+    public void beforeExecute(){
+        System.out.println("before execute task.");
+    }
+
+    public void afterExecute(){
+        System.out.println("after execute task.");
+    }
+
+}
